d4426d884af734d7fc071dba6b9c4f47ecc259c2
NovanTrihananto <152064002+NovanTrihananto@users.noreply.github.com>
2025-06-17 20:53:58 +0700

    Update Barangcontroller.js

--- controllers/Barangcontroller.js
+++ controllers/Barangcontroller.js
@@ -1,5 +1,10 @@
 import barang from "../model/Barangmodel.js";
+import { Storage } from "@google-cloud/storage";
+import { format } from "util";
 
+// Inisialisasi Storage
+const storage = new Storage();
+const bucket = storage.bucket(process.env.d01_7); 
 // GET all barang
 export const getBarang = async (req, res) => {
   try {
@@ -14,9 +19,7 @@ export const getBarang = async (req, res) => {
 export const getBarangById = async (req, res) => {
   try {
     const foundBarang = await barang.findOne({
-      where: {
-        id: req.params.id,
-      },
+      where: { id: req.params.id },
     });
     if (!foundBarang) return res.status(404).json({ message: "Barang tidak ditemukan" });
     res.status(200).json(foundBarang);
@@ -30,18 +33,38 @@ export const createBarang = async (req, res) => {
   const { Nama, harga, Deskripsi, Kategori } = req.body;
   const file = req.file;
 
-  if (!file) {
-    return res.status(400).json({ message: "Gambar tidak ditemukan" });
-  }
+  if (!file) return res.status(400).json({ message: "Gambar tidak ditemukan" });
 
   try {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    const blob = bucket.file(fileName);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+      contentType: file.mimetype,
+    });
+
+    await new Promise((resolve, reject) => {
+      blobStream.on("error", (err) => {
+        console.error("Upload Error:", err.message);
+        reject(new Error("Gagal upload file ke GCS"));
+      });
+      blobStream.on("finish", resolve);
+      blobStream.end(file.buffer);
+    });
+
+    // Buat file jadi public
+    await blob.makePublic();
+
+    const imageUrl = format(`https://storage.googleapis.com/${bucket.name}/${fileName}`);
+
     await barang.create({
       Nama,
       harga,
-      Img: file ? file.filename : null, // hanya simpan nama file, bukan path penuh
+      Img: imageUrl,
       Deskripsi,
       Kategori,
     });
+
     res.status(201).json({ message: "Barang berhasil ditambahkan" });
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -53,10 +76,9 @@ export const updateBarang = async (req, res) => {
   const { Nama, harga, Img, Deskripsi, Kategori } = req.body;
   try {
     const existingBarang = await barang.findOne({
-      where: {
-        id: req.params.id,
-      },
+      where: { id: req.params.id },
     });
+
     if (!existingBarang) return res.status(404).json({ message: "Barang tidak ditemukan" });
 
     await existingBarang.update({
@@ -66,6 +88,7 @@ export const updateBarang = async (req, res) => {
       Deskripsi,
       Kategori,
     });
+
     res.status(200).json({ message: "Barang berhasil diperbarui" });
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -76,10 +99,9 @@ export const updateBarang = async (req, res) => {
 export const deleteBarang = async (req, res) => {
   try {
     const existingBarang = await barang.findOne({
-      where: {
-        id: req.params.id,
-      },
+      where: { id: req.params.id },
     });
+
     if (!existingBarang) return res.status(404).json({ message: "Barang tidak ditemukan" });
 
     await existingBarang.destroy();
