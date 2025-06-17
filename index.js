aa0e293bf5ab7263e265c366ed37c6a4a1a39d2e
NovanTrihananto <152064002+NovanTrihananto@users.noreply.github.com>
2025-06-18 00:44:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 // Middleware
 app.use(cors({
   credentials: true,
-  origin: '*'  // ganti jika frontend beda origin
+  origin: "http://34.121.31.134"  // ganti jika frontend beda origin
 }));
 app.use(express.json());
 
