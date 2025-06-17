e800d089ff6f7992d6924070914477339220e232
NovanTrihananto <152064002+NovanTrihananto@users.noreply.github.com>
2025-06-18 01:50:57 +0700

    Update assosiasi.js

--- model/assosiasi.js
+++ model/assosiasi.js
@@ -4,8 +4,8 @@ import barang from "./Barangmodel.js";
 import Nota from "./Belibarangmodel.js";
 
 
-User.hasMany(Nota, { foreignKey: "idUser", constraints:true, as: "User" });
-Nota.belongsTo(User, { foreignKey: "idUser", constraints:true, as: "User" });
+User.hasMany(Nota, { foreignKey: "idUser", constraints:true, as: "user" });
+Nota.belongsTo(User, { foreignKey: "idUser", constraints:true, as: "user" });
 
 
 barang.hasMany(Nota, { foreignKey: "idbarang", constraints:true, as: "barang" });
